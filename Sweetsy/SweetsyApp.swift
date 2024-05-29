f34f12d2bf7da0b339405ce0ed3ede566c224cc3
Chetan Dhowlaghar <chetan.getsmail@gmail.com>
2024-05-29 16:27:53 -0500

    Initial Commit

---
+++ Sweetsy/SweetsyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweetsyApp.swift
+//  Sweetsy
+//
+//  Created by Chetan Dhowlaghar on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct SweetsyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
